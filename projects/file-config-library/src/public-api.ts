8c716afcb0e98fafdc8bbc4c29b52834ff047a0a
aniketkute <117454251+aniketkute@users.noreply.github.com>
2026-01-30 17:28:41 +0530

    public api.ts file for header component

--- projects/file-config-library/src/public-api.ts
+++ projects/file-config-library/src/public-api.ts
@@ -4,4 +4,4 @@
 
 // export * from './lib/file-config-library.service';
 // export * from './lib/file-config-library.component';
-export * from './lib/file-config-library.component';
+export * from './lib/header/header.component';
