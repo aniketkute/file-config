d23b48d32e1f551fcc2d06df15f96d87dd619844
aniketkute <117454251+aniketkute@users.noreply.github.com>
2026-02-04 14:39:59 +0530

    dist file

--- dist/file-config-library/lib/file-config-library.component.d.ts
+++ dist/file-config-library/lib/file-config-library.component.d.ts
@@ -1,8 +1,20 @@
+import { OnInit } from '@angular/core';
 import * as i0 from "@angular/core";
-export declare class DashboardUiComponent {
+export declare class DashboardUiComponent implements OnInit {
+    private masterService;
+    ngOnInit(): void;
+    BASE_URL: string;
     menus: any;
     styleSketchNumber: string | null;
+    selectedProcessName: string | null;
+    tableData: any[];
+    styleDetailsForDownload: any;
+    onInit(): void;
+    private getAllMaster;
     itemSelected(item: any): void;
+    prepareTableData(data: any): void;
+    enterPressed(): void;
+    downloadFile(file: any): void;
     static ɵfac: i0.ɵɵFactoryDeclaration<DashboardUiComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<DashboardUiComponent, "lib-file-config-library", never, {}, {}, never, never, true, never>;
+    static ɵcmp: i0.ɵɵComponentDeclaration<DashboardUiComponent, "lib-file-config-library", never, { "BASE_URL": { "alias": "BASE_URL"; "required": false; }; }, {}, never, never, true, never>;
 }
--- dist/file-config-library/lib/header/header.component.d.ts
+++
@@ -1,6 +0,0 @@
-import * as i0 from "@angular/core";
-export declare class HeaderComponent {
-    companyLogo: string | null;
-    static ɵfac: i0.ɵɵFactoryDeclaration<HeaderComponent, never>;
-    static ɵcmp: i0.ɵɵComponentDeclaration<HeaderComponent, "app-header", never, {}, {}, never, never, true, never>;
-}
