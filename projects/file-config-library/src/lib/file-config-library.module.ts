900ddf83e1d8c5cf734d425d6f60dca63017cbe9
GVenkata98 <venkatacalapathi7@gmail.com>
2026-04-24 15:02:27 +0530

    add FileConfigLibraryModule and update public API surface

---
+++ projects/file-config-library/src/lib/file-config-library.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from "@angular/core";
+import { DashboardUiComponent } from "file-config-library";
+
+
+@NgModule({
+    imports: [DashboardUiComponent],
+    exports: [DashboardUiComponent],
+})
+export class FileConfigLibraryModule {}
