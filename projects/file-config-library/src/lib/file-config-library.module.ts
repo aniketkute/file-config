9b98fc409f1e023f2246df7967975848adb8a98b
GVenkata98 <venkatacalapathi7@gmail.com>
2026-04-28 14:47:28 +0530

    feat: add image preview functionality and update styles for better UI

--- projects/file-config-library/src/lib/file-config-library.module.ts
+++ projects/file-config-library/src/lib/file-config-library.module.ts
@@ -4,11 +4,14 @@ import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { MatDialogModule } from "@angular/material/dialog";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from "@angular/material/icon";
 
 
 @NgModule({
     declarations: [DashboardUiComponent],
-    imports: [FormsModule, MatProgressBarModule, MatProgressSpinnerModule, CommonModule],
-    exports: [DashboardUiComponent],
+    imports: [FormsModule, MatProgressBarModule, MatProgressSpinnerModule, CommonModule, MatDialogModule, BrowserAnimationsModule, MatIconModule],
+    exports: [DashboardUiComponent, MatDialogModule],
 })
 export class FileConfigLibraryModule {}
