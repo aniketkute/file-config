7c91c6071a1bc070d8433197d337d862201912af
aniketkute <117454251+aniketkute@users.noreply.github.com>
2026-05-07 18:16:28 +0530

    module file changes=d

--- projects/file-config-library/src/lib/file-config-library.module.ts
+++ projects/file-config-library/src/lib/file-config-library.module.ts
@@ -1,17 +1,24 @@
-import { NgModule } from "@angular/core";
-import { DashboardUiComponent } from "./file-config-library.component";
+import { NgModule } from '@angular/core';
+import { DashboardUiComponent } from './file-config-library.component';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { MatDialogModule } from "@angular/material/dialog";
+import { MatDialogModule } from '@angular/material/dialog';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatIconModule } from "@angular/material/icon";
-
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-    declarations: [DashboardUiComponent],
-    imports: [FormsModule, MatProgressBarModule, MatProgressSpinnerModule, CommonModule, MatDialogModule, BrowserAnimationsModule, MatIconModule],
-    exports: [DashboardUiComponent, MatDialogModule],
+  declarations: [DashboardUiComponent],
+  imports: [
+    FormsModule,
+    MatProgressBarModule,
+    MatProgressSpinnerModule,
+    CommonModule,
+    MatDialogModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+  ],
+  exports: [DashboardUiComponent, MatDialogModule],
 })
 export class FileConfigLibraryModule {}
