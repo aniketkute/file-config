89e2d3f7b4e26ee4628ee3cdf8b637f805af65eb
aniketkute <117454251+aniketkute@users.noreply.github.com>
2026-05-07 14:51:01 +0530

    changes in file config changes

--- projects/file-config-library/src/lib/services/master.service.ts
+++ projects/file-config-library/src/lib/services/master.service.ts
@@ -8,24 +8,24 @@ import { map, switchMap, catchError, finalize } from 'rxjs/operators';
 })
 export class MasterService {
   readonly http = inject(HttpClient);
+  BASE_URL1 = 'http://192.168.54.189:4000';
 
   //#region POST
   getDetailsWithProcess(payload: any, BASE_URL: string) {
     return this.http.post(`${BASE_URL}/getFilesPath/v4`, payload);
   }
 
-  getDetailsWithProcessForPd(number: string, BASE_URL: string) {
-    return this.http.get(`${BASE_URL}/pd/file/getImagev2?number=${number}`);
+  getDetailsFromVersion(payload: any, BASE_URL: string) {
+    return this.http.post(`${BASE_URL}/getfilesUrlByMultiBarcode`, payload);
   }
+
   //#endregion
-
-
 
   // #region Api Calling and data setting.
   getFullDetails(
     styleSketchNumber: string,
     BASE_URL: string,
-    PD_BASE_URL: string
+    PD_BASE_URL: string,
   ): Observable<any> {
     const payload = {
       packetBarCode: styleSketchNumber,
@@ -44,39 +44,37 @@ export class MasterService {
             }))
           : [];
 
-        return this
-          .getDetailsWithProcessForPd(styleSketchNumber, PD_BASE_URL)
-          .pipe(
-            map((pdRes: any) => {
-              const pdData = pdRes?.data || {};
-              const pdMenus: any[] = [];
+        return this.getDetailsWithProcessForPd(
+          styleSketchNumber,
+          PD_BASE_URL,
+        ).pipe(
+          map((pdRes: any) => {
+            const pdData = pdRes?.data || {};
+            const pdMenus: any[] = [];
 
-              if (pdData['briefImage']) {
-                pdMenus.push({ processName: 'Brief Images', isSelected: false });
-              }
+            if (pdData['briefImage']) {
+              pdMenus.push({ processName: 'Brief Images', isSelected: false });
+            }
 
-              if (pdData['subBriefImage']) {
-                pdMenus.push({
-                  processName: 'Sub Brief Images',
-                  isSelected: false,
-                });
-              }
+            if (pdData['subBriefImage']) {
+              pdMenus.push({
+                processName: 'Sub Brief Images',
+                isSelected: false,
+              });
+            }
 
-              if (
-                !styleDetails?.filePaths?.['sketch'] &&
-                pdData['sketchImage']
-              ) {
-                pdMenus.push({ processName: 'sketch', isSelected: false });
-              }
+            if (!styleDetails?.filePaths?.['sketch'] && pdData['sketchImage']) {
+              pdMenus.push({ processName: 'sketch', isSelected: false });
+            }
 
-              return {
-                styleDetails,
-                menus,
-                pdData,
-                pdMenus,
-              };
-            })
-          );
+            return {
+              styleDetails,
+              menus,
+              pdData,
+              pdMenus,
+            };
+          }),
+        );
       }),
       catchError(() =>
         of({
@@ -84,8 +82,28 @@ export class MasterService {
           menus: [],
           pdData: [],
           pdMenus: [],
-        })
-      )
+        }),
+      ),
+    );
+  }
+
+  getVersionDetailsFromPd(
+    styleNo: string,
+    PD_URL: string,
+    refFromEmrGatiDiaVendor: boolean,
+  ) {
+    return this.http.get(
+      `${PD_URL}/pd/file/getVersionByStyleNumber?number=${styleNo}&refFromEmrGatiDiaVendor=${refFromEmrGatiDiaVendor}`,
+    );
+  }
+
+  getDetailsWithProcessForPd(number: string, BASE_URL: string) {
+    return this.http.get(`${BASE_URL}/pd/file/getImagev2?number=${number}`);
+  }
+
+  getSyleVersionFromBag(MFG_BASE_URL: string, bagNo: string) {
+    return this.http.get(
+      `${MFG_BASE_URL}/asset/v1/bag/getStyleVersionByBagNo?bagNo=${bagNo}`,
     );
   }
 }
